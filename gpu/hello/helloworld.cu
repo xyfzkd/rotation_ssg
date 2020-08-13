b1ab467a30c4d5a32c6cbd2ca21fe3e69b9ac77b
XieYufeng <xyfzkd@outlook.com>
2020-08-13 17:37:32 +0800

    cuda_demo

---
+++ gpu/hello/helloworld.cu
@@ -0,0 +1,22 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+#include <Windows.h>
+__global__ void helloFromGPU(void)
+{
+        printf("Hello World from GPU!\n");
+        
+}
+
+int main(void)
+{
+        // hello from cpu
+        cudaError_t cudaStatus;
+            printf("Hello World from CPU!\n");
+
+                helloFromGPU << <1, 10 >> > ();
+                    cudaDeviceReset();//重置CUDA设备释放程序占用的资源
+                        system("pause");
+                            return 0;
+                            
+}
