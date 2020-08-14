dac546a7065b18f24324cbc575bbef6199195a57
XieYufeng <xyfzkd@outlook.com>
2020-08-14 11:17:18 +0800

    patch_gpu_option

---
+++ src/acc/cuda/cuda_alignPatch.cu
@@ -0,0 +1,47 @@
+#include <cufftw.h>
+#include <cufft.h>
+#include "cuda_runtime.h"
+#include "src/acc/alignPatch.h"
+
+
+void CuFFT::inverseFourierTransform(
+        MultidimArray<fComplex>& src,
+        MultidimArray<float>& dest)
+{
+    if (!areSizesCompatible(dest, src))
+    {
+        resizeRealToMatch(dest, src);
+    }
+
+    MultidimArray<fComplex> src2 = src;
+
+    std::vector<int> N(0);
+    if (dest.zdim > 1) N.push_back(dest.zdim);
+    if (dest.ydim > 1) N.push_back(dest.ydim);
+    N.push_back(dest.xdim);
+
+    /* https://docs.nvidia.com/cuda/cufft/index.html#cufftdoublecomplex 4.2.1 */
+    cufftHandle planIn;
+    cufftComplex *data;
+
+    cudaMalloc((void**)&data, sizeof(cufftComplex)*N[0]*(N[1]/2+1));
+    if (cudaGetLastError() != cudaSuccess){
+        fprintf(stderr, "Cuda error: Failed to allocate\n");
+        return;
+    }
+
+    /* Create a 2D FFT plan. */
+    cufftPlan2d(&planIn,  N[0], N[1], CUFFT_C2R);
+
+    /* https://stackoverflow.com/questions/16511526/cufft-and-fftw-data-structures-are-cufftcomplex-and-fftwf-complex-interchangabl
+     * Are cufftComplex and fftwf_complex interchangable? yes!
+     */
+
+    /* https://docs.nvidia.com/cuda/cufft/index.html 3.9.3 */
+
+    if (cufftExecC2R(planIn, (cufftComplex*) MULTIDIM_ARRAY(src2), MULTIDIM_ARRAY(dest)) != CUFFT_SUCCESS){
+        fprintf(stderr, "CUFFT Error: Unable to execute plan\n");
+        return;
+    }
+    cudaFree(data);
+}
