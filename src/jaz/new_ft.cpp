0834a7f1f315c0eb98c28368105e9a30804f65ec
XieYufeng <xyfzkd@outlook.com>
2020-08-16 01:48:37 +0800

    gpubug

--- src/jaz/new_ft.cpp
+++ src/jaz/new_ft.cpp
@@ -493,17 +493,18 @@ NewFFT::FloatPlan::FloatPlan(
 
 	pthread_mutex_lock(&fftw_plan_mutex_new);
     print_comp_image(complex);
-	fftwf_plan planForward = fftwf_plan_dft_r2c(
-			ndim, &N[0],
-			MULTIDIM_ARRAY(real),
-			(fftwf_complex*) MULTIDIM_ARRAY(complex),
-			flags);
+    fftwf_plan planBackward = fftwf_plan_dft_c2r(
+            ndim, &N[0],
+            (fftwf_complex*) MULTIDIM_ARRAY(complex),
+            MULTIDIM_ARRAY(real),
+            flags);
+
     print_comp_image(complex);
-	fftwf_plan planBackward = fftwf_plan_dft_c2r(
-			ndim, &N[0],
-			(fftwf_complex*) MULTIDIM_ARRAY(complex),
-			MULTIDIM_ARRAY(real),
-			flags);
+    fftwf_plan planForward = fftwf_plan_dft_r2c(
+            ndim, &N[0],
+            MULTIDIM_ARRAY(real),
+            (fftwf_complex*) MULTIDIM_ARRAY(complex),
+            flags);
 
 	pthread_mutex_unlock(&fftw_plan_mutex_new);
 	
