1f1328241b482686807858ad462b8801b4516215
XieYufeng <xyfzkd@outlook.com>
2020-08-16 00:47:01 +0800

    gpubug

--- src/motioncorr_runner.cpp
+++ src/motioncorr_runner.cpp
@@ -1720,9 +1720,11 @@ bool MotioncorrRunner::test(){
     if(do_gpu){
 
         RCTIC(TIMING_GPU_IFFT);
+        print_comp_image(Fccs);
         CuFFT::inverseFourierTransform(Fccs, Iccs());
         RCTIC(TIMING_GPU_IFFT);
         RCTIC(TIMING_CPU_IFFT);
+        print_comp_image(Fccs);
         NewFFT::inverseFourierTransform(Fccs, Iccs2());
         RCTOC(TIMING_CPU_IFFT);
         diff(Iccs2(), Iccs());
