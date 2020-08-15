d9fc3a5ef4ae3255ca260bfccfcf0a2cf1d23baa
XieYufeng <xyfzkd@outlook.com>
2020-08-15 16:06:17 +0800

    gpubug

--- src/acc/cuda/cuda_alignPatch.cu
+++ src/acc/cuda/cuda_alignPatch.cu
@@ -4,14 +4,61 @@
 //#include "src/acc/cuda/cuda_alignPatch.h"
 #include "src/acc/acc_alignPatch.h"
 
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "device_launch_parameters.h"
 
+#define TEST
+
+#define pi 3.1415926535
+#define LENGTH 100 //signal sampling points
 
 
 void CuFFT::inverseFourierTransform(
         MultidimArray<fComplex>& src,
         MultidimArray<float>& dest)
 {
+#ifdef TEST
+    float Data[LENGTH] = { 1,2,3,4 };
+    float fs = 1000000.000;//sampling frequency
+    float f0 = 200000.00;// signal frequency
+    for (int i = 0; i < LENGTH; i++)
+    {
+        Data[i] = 1.35*cos(2 * pi*f0*i / fs);//signal gen,
+    }
 
+    cufftComplex *CompData = (cufftComplex*)malloc(LENGTH * sizeof(cufftComplex));//allocate memory for the data in host
+    int i;
+    for (i = 0; i < LENGTH; i++)
+    {
+        CompData[i].x = Data[i];
+        CompData[i].y = 0;
+    }
+
+    cufftComplex *d_fftData;
+    cudaMalloc((void**)&d_fftData, LENGTH * sizeof(cufftComplex));// allocate memory for the data in device
+    cudaMemcpy(d_fftData, CompData, LENGTH * sizeof(cufftComplex), cudaMemcpyHostToDevice);// copy data from host to device
+
+    cufftHandle plan;// cuda library function handle
+    cufftPlan1d(&plan, LENGTH, CUFFT_C2C, 1);//declaration
+    cufftExecC2C(plan, (cufftComplex*)d_fftData, (cufftComplex*)d_fftData, CUFFT_FORWARD);//execute
+    cudaDeviceSynchronize();//wait to be done
+    cudaMemcpy(CompData, d_fftData, LENGTH * sizeof(cufftComplex), cudaMemcpyDeviceToHost);// copy the result from device to host
+
+    for (i = 0; i < LENGTH / 2; i++)
+    {
+        printf("i=%d\tf= %6.1fHz\tRealAmp=%3.1f\t", i, fs*i / LENGTH, CompData[i].x*2.0 / LENGTH);
+        printf("ImagAmp=+%3.1fi", CompData[i].y*2.0 / LENGTH);
+        printf("\n");
+    }
+    cufftDestroy(plan);
+    free(CompData);
+    cudaFree(d_fftData);
+#endif
+#ifdef GPU
     if (!areSizesCompatible(dest, src))
     {
         resizeRealToMatch(dest, src);
@@ -56,5 +103,5 @@ void CuFFT::inverseFourierTransform(
 
     cudaFree(comp_data);
     cudaFree(real_data);
-
+#endif
 }
